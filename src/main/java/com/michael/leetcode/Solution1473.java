b09f492d04c37a795cadac552ce97f142d69d099
limingming <02048073@yto.net.cn>
2020-11-11 14:38:06 +0800

    提交

---
+++ src/main/java/com/michael/leetcode/Solution1473.java
@@ -0,0 +1,56 @@
+package com.michael.leetcode;
+
+import org.junit.jupiter.api.Test;
+/*
+[1473] 1470. 重新排列数组
+给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+ 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+
+ 示例 1：
+ 输入：nums = [2,5,1,3,4,7], n = 3
+输出：[2,3,5,4,1,7]
+解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+
+ 示例 2：
+ 输入：nums = [1,2,3,4,4,3,2,1], n = 4
+输出：[1,4,2,3,3,2,4,1]
+
+ 示例 3：
+ 输入：nums = [1,1,2,2], n = 2
+输出：[1,2,1,2]
+
+ 提示：
+ 1 <= n <= 500
+ nums.length == 2n
+ 1 <= nums[i] <= 10^3
+ */
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 思路 双指针 时间复杂度为 O(n)
+public class Solution1473 {
+    public int[] shuffle(int[] nums, int n) {
+       int[] result = new int[2*n];
+       int j = 0;
+       int k = n;
+        for (int i = 0; i < result.length; i++) {
+           if (i%2==0){
+               result[i] = nums[j];
+               j++;
+           }else {
+               result[i] = nums[k];
+               k++;
+           }
+        }
+        return result;
+
+    }
+    @Test
+    public void test(){
+        int[] nums = {2,5,1,3,4,7};
+        int n = 3;
+        int[] shuffle = shuffle(nums, n);
+        System.out.println("");
+    }
+
+}
